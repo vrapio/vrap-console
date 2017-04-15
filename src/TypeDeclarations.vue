f0b17cf54fe99aa1998f35701b8b04326dc2f34b
katmatt <matthias.koester@gmail.com>
2017-04-15 19:28:21 +0200

    Panels are back.

--- src/TypeDeclarations.vue
+++ src/TypeDeclarations.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
-        <h4>{{title}}</h4>
-        <div v-if="typeDeclarations" class="form">
+        <h4><strong>{{title}}</strong></h4>
+        <div v-if="typeDeclarations" class="form-horizontal">
             <div v-for="typeDeclaration in typeDeclarations" class="form-group">
-                <label :for="typeDeclaration.name">{{typeDeclaration.name}}</label>
-                <div :id="typeDeclaration.name" class="input-group">
-                    <input class="form-control pull-left" type="text" :placeholder="typeDeclaration.example">
-                    <span v-if="typeDeclaration.example" class="input-group-addon glyphicon glyphicon-paste" id="basic-addon1"></span>
+                <label :for="typeDeclaration.name" class="col-sm-1 control-label">{{typeDeclaration.name}}</label>
+                <div class="input-group col-sm-4">
+                    <input class="form-control" type="text" :placeholder="typeDeclaration.example">
+                    <span v-if="typeDeclaration.example" class="input-group-addon glyphicon glyphicon-paste"
+                          id="basic-addon1"></span>
                 </div>
             </div>
         </div>
--- src/VrapConsole.vue
+++ src/VrapConsole.vue
@@ -2,13 +2,18 @@
     <div id="vrap-console">
         <navbar v-on:select="onSelect" :resource="resource"></navbar>
         <resource-panel :resource="resource"></resource-panel>
-        <template v-for="method in resource.methods">
-            <div>
-                <h4 class="text-capitalize"><strong>{{method.method}}</strong></h4>
+        <p></p>
+        <div v-for="method in resource.methods" class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title"><strong class="text-uppercase">{{method.method}}</strong></h4>
+            </div>
+            <div class="panel-body">
                 <type-declarations title="Query Parameters" :typeDeclarations="method.queryParams"></type-declarations>
+            </div>
+            <div class="panel-footer">
                 <button type="button" class="btn btn-success text-capitalize">{{method.method}}</button>
             </div>
-        </template>
+        </div>
     </div>
 </template>
 <script src="./VrapConsole.js"></script>
